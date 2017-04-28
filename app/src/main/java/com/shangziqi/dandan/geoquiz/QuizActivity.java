97b48d5ad0071b320d31337b912091b5b0b4716f
andy-bst <andy@bluestacks.com>
2017-04-28 17:26:22 +0800

    Challenge

--- app/src/main/java/com/shangziqi/dandan/geoquiz/QuizActivity.java
+++ app/src/main/java/com/shangziqi/dandan/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.shangziqi.dandan.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -31,8 +32,15 @@ public class QuizActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
-            }
+//                Toast t = new Toast(QuizActivity.this);
+//                t.setGravity(Gravity.TOP, 0, 0);
+//                t.setDuration(Toast.LENGTH_SHORT);
+//                t.setText(R.string.incorrect_toast);
+//                t.show();
+                Toast t = Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT);
+                t.setGravity(Gravity.TOP, 0, 0);
+                t.show();
+        }
         });
 
 
