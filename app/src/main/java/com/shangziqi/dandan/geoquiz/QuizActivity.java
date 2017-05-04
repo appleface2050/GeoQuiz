719a47da757e6ba69fca08b612d8ee5d52fd0530
andy-bst <andy@bluestacks.com>
2017-05-04 15:17:20 +0800

    chapter2

--- app/src/main/java/com/shangziqi/dandan/geoquiz/QuizActivity.java
+++ app/src/main/java/com/shangziqi/dandan/geoquiz/QuizActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 //
 //        import static android.widget.Toast.makeText;
@@ -13,18 +14,56 @@ public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    private Question[] mQuestionBank = new Question[]{
+            new Question(R.string.question_australia, true),
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true),
+    };
+
+    private int mCurrentIndex = 0;
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+        int messageResId = 0;
+        if (userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        mQuestionTextView = (TextView) findViewById(R.id.textView);
+//        int question = mQuestionBank[mCurrentIndex].getTextResId();
+//        mQuestionTextView.setText(question);
+
+
         mTrueButton = (Button) findViewById(R.id.button2);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+//                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            checkAnswer(true);
             }
+
         });
 
         mFalseButton = (Button) findViewById(R.id.button);
@@ -32,17 +71,23 @@ public class QuizActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-//                Toast t = new Toast(QuizActivity.this);
+                checkAnswer(false);
+//                Toast t = Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT);
 //                t.setGravity(Gravity.TOP, 0, 0);
-//                t.setDuration(Toast.LENGTH_SHORT);
-//                t.setText(R.string.incorrect_toast);
 //                t.show();
-                Toast t = Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT);
-                t.setGravity(Gravity.TOP, 0, 0);
-                t.show();
-        }
+            }
         });
 
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+//                int question = mQuestionBank[mCurrentIndex].getTextResId();
+//                mQuestionTextView.setText(question);
+                updateQuestion();
+            }
+        });
     }
 }
